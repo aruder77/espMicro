0f00a001d63fad74af9e7bf6f8c67d615920c10f
Axel Ruder <axel.ruder@gmail.com>
2021-04-09 21:48:30 +0200

    copyExternalLibs

--- copyExternalLibs.sh
+++ copyExternalLibs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-cp ../microhomie/
+
+# copy microhomie
+cp -r ../microhomie/homie lib/
+cp -r ../microhomie/lib/primitives lib/
+cp ../microhomie/lib/mqtt_as.py lib/
+
+# copy wifimgr
+cp ../WiFiManager/wifimgr.py lib/
+
+# copy ota_updater
+cp ../micropython-ota-updater/app/ota_updater.py lib/
+cp ../micropython-ota-updater/app/httpclient.py lib/
