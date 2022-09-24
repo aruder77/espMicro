3164b44b4d193000a0f4558b174f695a1b12cc90
Axel Ruder <axel.ruder@gmail.com>
2022-09-24 12:01:47 +0200

    adopted to EspVent 2.0 ports

--- app/espvent_device.py
+++ app/espvent_device.py
@@ -10,14 +10,14 @@ class EspVentDevice(HomieDevice):
         super().__init__(settings)
 
         motorNodes = [
-            MotorNode(4, 1, False), 
-            MotorNode(13, 2, True),
-            MotorNode(14, 3, False),
-            MotorNode(27, 4, True),
-            MotorNode(26, 5, False),
-            MotorNode(25, 6, True), 
-            MotorNode(33, 7, False), 
-            MotorNode(32, 8, True) 
+            MotorNode(33, 1, False), 
+            MotorNode(32, 2, True),
+            MotorNode(17, 3, False),
+            MotorNode(16, 4, True),
+            MotorNode(13, 5, False),
+            MotorNode(27, 6, True), 
+            MotorNode(26, 7, False), 
+            MotorNode(25, 8, True) 
         ]
         for motorNode in motorNodes:
             self.add_node(motorNode)
